268d6d259b0a7c44efc95be23071724b5e7da3f1
Jason Kirshner <jasonk@ruckusmarketing.com>
2024-05-11 22:20:41 -0400

    Pass hero props
    Signed-off-by: Jason Kirshner <jasonk@ruckusmarketing.com>

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,20 @@ import FooterCTA from "@/components/FooterCTA/FooterCTA";
 import Hero from "@/components/Hero/Hero";
 import Services from "@/components/Services/Services";
 import Testimonials from "@/components/Testimonials/Testimonials";
-import Image from "next/image";
+
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Hero />
+      <Hero
+        imageUrl="/hero_right.png"
+        bgImage="/hero_bg.png"
+        title="Expert Dog Training"
+        description="Yin Yang K9 is based in San Diego, California and offers a variety of private in home dog training, day training and group classes in and around San Diego."
+        buttonText1="Contact Us"
+        buttonText2="About Us"
+      />
       <Testimonials />
       <Services />
       <FooterCTA />
