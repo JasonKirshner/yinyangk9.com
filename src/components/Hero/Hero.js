530485ccf7997f8a4170a8eea0bd927524125d75
Jason Kirshner <jasonk@ruckusmarketing.com>
2024-05-11 22:21:06 -0400

    Build Hero
    Signed-off-by: Jason Kirshner <jasonk@ruckusmarketing.com>

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,20 +1,22 @@
 // components/Hero.js
 
+import Link from "next/link";
 import styles from "./Hero.module.css";
 
-const Hero = ({ title, description, buttonText1, buttonText2, imageUrl }) => {
+const Hero = ({ title, description, buttonText1, buttonText2, imageUrl, bgImage }) => {
   return (
-    <div className="hero">
-      <div className="content">
-        <h1>{title}</h1>
-        <p>{description}</p>
-        <div className="buttons">
-          <button>{buttonText1}</button>
-          <button>{buttonText2}</button>
+    <div className={styles.hero}>
+      <img src={bgImage} alt="Hero Background Image" className={styles.bgImage} />
+      <div className={`${styles.heroContainer} container`}>
+        <div className={styles.content}>
+          <h1 className={`h1 ${styles.title}`}>{title}</h1>
+          <p className={`h2 ${styles.description}`}>{description}</p>
+          <div className={styles.buttons}>
+            <Link className={`button ${styles.contactUsBtn}`} href="/contact">{buttonText1}</Link>
+            <Link className={`button button--alt ${styles.aboutUsBtn}`} href="/about">{buttonText2}</Link>
+          </div>
         </div>
-      </div>
-      <div className="image-container">
-        {/* <img src={imageUrl} alt="Hero Image" /> */}
+        <img src={imageUrl} alt="Hero Image" className={styles.image} />
       </div>
     </div>
   );
