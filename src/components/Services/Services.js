f44b97967e9f4e0b3d7a5899939e5499ba722ef4
Jason Kirshner <jasonk@ruckusmarketing.com>
2024-06-12 22:04:12 -0400

    Build services
    Signed-off-by: Jason Kirshner <jasonk@ruckusmarketing.com>

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,34 +1,25 @@
-// components/Services.js
+import styles from './Services.module.css';
 
 import Service from '@/components/Service/Service';
 
-const Services = () => {
+const Services = ({ title, services }) => {
+  const renderServices = () => services.map((service, i) => (
+    <Service
+      key={i}
+      id={i}
+      title={service.name}
+      description={service.shortDescription}
+    />
+  ))
+
   return (
-    <div className="services">
-      <Service
-        title="Service 1"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-        backgroundColor="#f2f2f2"
-        borderColor="#ccc"
-      />
-      <Service
-        title="Service 2"
-        description="Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
-        backgroundColor="#fff"
-        borderColor="#ccc"
-      />
-      <Service
-        title="Service 3"
-        description="Fusce euismod consequat ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-        backgroundColor="#f2f2f2"
-        borderColor="#ccc"
-      />
-      <Service
-        title="Service 4"
-        description="Sed non mauris vitae erat consequat auctor eu in elit."
-        backgroundColor="#fff"
-        borderColor="#ccc"
-      />
+    <div className={styles.services}>
+      <div className={`container ${styles.servicesContainer}`}>
+        <h3 className={styles.servicesTitle}>{title}</h3>
+        <div className={styles.servicesGrid}>
+          {renderServices()}
+        </div>
+      </div>
     </div>
   );
 };
