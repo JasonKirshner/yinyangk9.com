09010dc99086ebda2285e15b4e2efd8d31e67c46
Jason Kirshner <jasonkirshner13@gmail.com>
2024-07-08 23:00:32 -0400

    Use next/image component for hero images
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,12 @@
-// components/Hero.js
-
+import Image from "next/image";
 import Link from "next/link";
+
 import styles from "./Hero.module.css";
 
-const Hero = ({ title, description, buttonText1, buttonText2, imageUrl, bgImage }) => {
+const Hero = ({ title, description, buttonText1, buttonText2, image, bgImage }) => {
   return (
     <div className={styles.hero}>
-      <img src={bgImage} alt="Hero Background Image" className={styles.bgImage} />
+      <Image src={bgImage} alt="Hero Background Image" className={styles.bgImage} />
       <div className={`${styles.heroContainer} container`}>
         <div className={styles.content}>
           <h1 className={`h1 ${styles.title}`}>{title}</h1>
@@ -16,7 +16,7 @@ const Hero = ({ title, description, buttonText1, buttonText2, imageUrl, bgImage 
             <Link className={`button button--alt ${styles.aboutUsBtn}`} href="/about">{buttonText2}</Link>
           </div>
         </div>
-        <img src={imageUrl} alt="Hero Image" className={styles.image} />
+        <Image src={image} alt="Hero Image" className={styles.image} />
       </div>
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,9 @@ import testimonials from '@/lib/data/testimonials.json';
 import services from '@/lib/data/services.json';
 import { validation } from '@/lib/js/util'
 
+import heroImage from '../../public/hero_right.png'
+import bgImage from '../../public/hero_bg.png'
+
 export async function getServerSideProps({ req, res }) {
   const instagramFeed = await getInstagramFeed(req, res)
 
@@ -19,8 +22,8 @@ export default function Home({ instagramFeed }) {
   return (
     <main>
       <Hero
-        imageUrl="/hero_right.png"
-        bgImage="/hero_bg.png"
+        image={heroImage}
+        bgImage={bgImage}
         title="Expert Dog Training"
         description="Yin Yang K9 is based in San Diego, California and offers a variety of private in home dog training, day training and group classes in and around San Diego."
         buttonText1="Contact Us"
