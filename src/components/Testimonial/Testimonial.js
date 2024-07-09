c22c3b8670f2afbdf1bc0a0ca561f6edc7643f8e
Jason Kirshner <jasonkirshner13@gmail.com>
2024-07-09 00:46:54 -0400

    Use next/image in testimonials
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -1,12 +1,16 @@
+import Image from 'next/image';
+
+import mouthpieceImage from '../../../public/mouthpiece.png'
+
 import styles from './Testimonial.module.css';
 
 const Testimonial = ({ name, text, image }) => {
   return (
     <div className={styles.testimonial}>
-      <img src={image} className={styles.avatar} />
+      <Image src={image} className={styles.avatar} alt={`${name} Image`} placeholder='blur' />
       <p className={styles.text}>{text}</p>
       <p className={styles.name}>{name}</p>
-      <img src="./mouthpiece.png" className={styles.mouthpiece} />
+      <Image src={mouthpieceImage} className={styles.mouthpiece} alt={'Chat bubble mouthpiece image'} placeholder='blur' />
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,30 @@ import Testimonials from "@/components/Testimonials/Testimonials";
 import InstagramFeed from "@/components/InstagramFeed/InstagramFeed"
 
 import getInstagramFeed from "@/lib/resources/InstagramBasicDisplayClient";
-import testimonials from '@/lib/data/testimonials.json';
 import services from '@/lib/data/services.json';
 import { validation } from '@/lib/js/util'
 
 import heroImage from '../../public/hero_right.png'
 import bgImage from '../../public/hero_bg.png'
+import avatarImage from '../../public/avatar.png'
+
+const testimonials = [
+  {
+    image: avatarImage,
+    testimony: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+    name: "John Doe"
+  },
+  {
+    image: avatarImage,
+    testimony: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+    name: "John Doe"
+  },
+  {
+    image: avatarImage,
+    testimony: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+    name: "John Doe"
+  }
+]
 
 export async function getServerSideProps({ req, res }) {
   const instagramFeed = await getInstagramFeed(req, res)
