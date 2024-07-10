454411f5824069493855d2b7288d0eb4fc4ee5fd
Jason Kirshner <jasonkirshner13@gmail.com>
2024-07-10 01:29:17 -0400

    Update footer
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,9 @@
 import Link from 'next/link'
-import Image from "next/image";
+import Image from "next/image"
 
 import heroImage from '../../../public/hero_bg.png'
 import mailImage from '../../../public/mail.png'
 import phoneImage from '../../../public/phone.png'
-import fbImage from '../../../public/facebook.png'
 import igImage from '../../../public/instagram.png'
 import logo from '../../../public/logo.png'
 
@@ -17,35 +16,27 @@ const Footer = () => {
       <div className={`${styles.footerContainer} container`}>
         <div className={styles.footerLeft}>
           <Link className={styles.homeLink} href="/">
-            <Image src={logo} alt="Logo" className={styles.logo} placeholder='blur' />
+            <Image src={logo} alt="Logo" className={styles.logo} />
           </Link>
           <p className={styles.copyright}>© 2024 Yin Yang K9. All Rights Reserved</p>
         </div>
         <div className={styles.footerRight}>
           <Link href="mailto:yinyangk9@gmail.com" className={styles.contactLink}>
-            <Image src={mailImage} alt="Email Icon" className={styles.contactIcon} placeholder='blur' />
+            <Image src={mailImage} alt="Email Icon" className={styles.contactIcon} />
             <p className={styles.contactLabel}>yinyangk9@gmail.com</p>
           </Link>
           <Link href="tel:+(760) 855-5667" className={styles.contactLink}>
-            <Image src={phoneImage} alt="Phone Icon" className={styles.contactIcon} placeholder='blur' />
+            <Image src={phoneImage} alt="Phone Icon" className={styles.contactIcon} />
             <p className={styles.contactLabel}>+(760) 855-5667</p>
           </Link>
           <Link href="https://www.instagram.com/yinyangk9/" className={styles.contactLink}>
-            <Image src={igImage} alt="Facebook Icon" className={styles.contactIcon} placeholder='blur' />
+            <Image src={igImage} alt="Facebook Icon" className={styles.contactIcon} />
             <p className={styles.contactLabel}>@yinyangk9</p>
           </Link>
-          {/* <div className={styles.socials}>
-            <Link href="http://facebook.com" className={styles.socialLink}>
-              <Image src={fbImage} alt="Facebook Icon" className={styles.socialIcon} />
-            </Link>
-            <Link href="https://www.instagram.com/yinyangk9/" className={styles.socialLink}>
-              <Image src={igImage} alt="Facebook Icon" className={styles.socialIcon} />
-            </Link>
-          </div> */}
         </div>
       </div>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
