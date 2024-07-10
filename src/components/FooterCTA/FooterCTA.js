7a81fcd8a3a148d6eb108cef9b6ade240001b1fd
Jason Kirshner <jasonkirshner13@gmail.com>
2024-07-10 01:37:57 -0400

    Use next/image
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- src/components/FooterCTA/FooterCTA.js
+++ src/components/FooterCTA/FooterCTA.js
@@ -1,5 +1,9 @@
-import styles from './FooterCTA.module.css';
+import styles from './FooterCTA.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
+
+import pawsLeft from '../../../public/paws_left.png'
+import pawsRight from '../../../public/paws_right.png'
 
 const FooterCTA = () => {
   return (
@@ -9,11 +13,11 @@ const FooterCTA = () => {
         <Link href="/contact" className={`button button--secondary ${styles.footerCTAbtn}`}>
           Contact Us
         </Link>
-        <img src="paws_left.png" alt="Paws Left" className={styles.pawsLeft} />
-        <img src="paws_right.png" alt="Paws Right" className={styles.pawsRight} />
+        <Image src={pawsLeft} alt="Paws Left" className={styles.pawsLeft} />
+        <Image src={pawsRight} alt="Paws Right" className={styles.pawsRight} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default FooterCTA;
+export default FooterCTA
