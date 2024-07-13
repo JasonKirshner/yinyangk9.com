a9126faa0c2b862a47bc361ddc6dded9ca9af83d
Jason Kirshner <jasonkirshner13@gmail.com>
2024-07-13 16:45:44 -0400

    Lazy load footercta images
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- src/components/FooterCTA/FooterCTA.js
+++ src/components/FooterCTA/FooterCTA.js
@@ -4,17 +4,22 @@ import Image from 'next/image'
 
 import pawsLeft from '../../../public/paws_left.png'
 import pawsRight from '../../../public/paws_right.png'
+import InViewLoad from '../InViewLoad/InViewLoad'
 
 const FooterCTA = () => {
   return (
     <div className={styles.footerCTA}>
       <div className={`${styles.footerCTAcontainer} container`}>
         <h2 className={`h1 ${styles.title}`}>We&apos;re Here To Help</h2>
-        <Link href="/contact" className={`button button--secondary ${styles.footerCTAbtn}`}>
+        <Link href='/contact' className={`button button--secondary ${styles.footerCTAbtn}`}>
           Contact Us
         </Link>
-        <Image src={pawsLeft} alt="Paws Left" className={styles.pawsLeft} />
-        <Image src={pawsRight} alt="Paws Right" className={styles.pawsRight} />
+        <InViewLoad>
+          <Image src={pawsLeft} alt='Paws Left' className={styles.pawsLeft} />
+        </InViewLoad>
+        <InViewLoad>
+          <Image src={pawsRight} alt='Paws Right' className={styles.pawsRight} />
+        </InViewLoad>
       </div>
     </div>
   )
