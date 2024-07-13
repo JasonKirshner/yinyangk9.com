f4413a1c2257bc9c292b5e24fbd59a4ad612ee3c
Jason Kirshner <jasonkirshner13@gmail.com>
2024-07-13 16:49:47 -0400

    Lazy load herosmall image
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- src/components/HeroSmall/HeroSmall.js
+++ src/components/HeroSmall/HeroSmall.js
@@ -1,12 +1,15 @@
 import Image from 'next/image'
 
 import styles from './HeroSmall.module.css'
+import InViewLoad from '../InViewLoad/InViewLoad'
 
 const HeroSmall = ({ headerText, backgroundImage }) => {
   return (
     <div className={styles.heroSmall}>
-      <Image src={backgroundImage} alt="Hero Small Background Image" className={styles.bgImage} priority />
-      <div className="container">
+      <InViewLoad>
+        <Image src={backgroundImage} alt='Hero Small Background Image' className={styles.bgImage} priority />
+      </InViewLoad>
+      <div className='container'>
         <h1 className={`${styles.headerText} h1`}>{headerText}</h1>
       </div>
     </div>
