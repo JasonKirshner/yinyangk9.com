a457c92fb7ff79bd1f3ea78658f7ee4b7f9f1570
Jason Kirshner <jasonkirshner13@gmail.com>
2024-07-13 15:37:58 -0400

    Lazy load navbard logo
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,6 +8,7 @@ import { Spin as Hamburger } from 'hamburger-react'
 import styles from './Navbar.module.css'
 
 import logo from '../../../public/logo.png'
+import InViewLoad from '../InViewLoad/InViewLoad'
 
 const Navbar = () => {
   const [isMenuOpen, setMenuOpen] = useState(false)
@@ -19,22 +20,24 @@ const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <div className={`${styles.navbarContainer} container`}>
-        <Link className={styles.homeLink} href="/">
-          <Image src={logo} alt="Logo" className={styles.logo} priority />
-        </Link>
-        <div className={`${styles.navbarLinks}` + (isMenuOpen ? ` ${styles.navbarOpen}` : '') }>
-          <Link className={styles.navLink} href="/about">
+        <InViewLoad>
+          <Link className={styles.homeLink} href='/'>
+            <Image src={logo} alt='Logo' className={styles.logo} priority />
+          </Link>
+        </InViewLoad>
+        <div className={`${styles.navbarLinks}` + (isMenuOpen ? ` ${styles.navbarOpen}` : '')}>
+          <Link className={styles.navLink} href='/about'>
             About
           </Link>
-          <Link className={styles.navLink} href="/services">
+          <Link className={styles.navLink} href='/services'>
             Services
           </Link>
-          <Link className={`${styles.navLink} button`} href="/contact">
+          <Link className={`${styles.navLink} button`} href='/contact'>
             Contact Us
           </Link>
         </div>
         <div className={styles.navBurger}>
-          <Hamburger toggle={toggleMenu} toggled={isMenuOpen} direction="right" color="#D52129" label="Show Menu" />
+          <Hamburger toggle={toggleMenu} toggled={isMenuOpen} direction='right' color='#D52129' label='Show Menu' />
         </div>
       </div>
     </nav>
