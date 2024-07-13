09d01e787abc5deddec229479404b74dab925816
Jason Kirshner <jasonkirshner13@gmail.com>
2024-07-13 16:18:43 -0400

    Lazy load testimonial pfp
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- src/components/InViewLoad/InViewLoad.js
+++ src/components/InViewLoad/InViewLoad.js
@@ -1,5 +1,5 @@
-import React from "react"
-import { InView } from "react-intersection-observer"
+import React from 'react'
+import { InView } from 'react-intersection-observer'
 
 import styles from './InViewLoad.module.css'
 
@@ -8,7 +8,7 @@ const InViewLoad = ({ children }) => {
     <InView triggerOnce>
       {({ inView, ref }) => (
         React.cloneElement(children, {
-          ref: ref,
+          ref,
           style: { opacity: inView ? 1 : 0 },
           className: `${children.props.className} ${styles.transition}`
         })
--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -3,14 +3,17 @@ import Image from 'next/image'
 import mouthpieceImage from '../../../public/mouthpiece.png'
 
 import styles from './Testimonial.module.css'
+import InViewLoad from '../InViewLoad/InViewLoad'
 
 const Testimonial = ({ name, text, image }) => {
   return (
     <div className={styles.testimonial}>
-      <Image src={image} className={styles.avatar} alt={`${name} Image`} placeholder='blur' />
+      <InViewLoad>
+        <Image src={image} className={styles.avatar} alt={`Image of ${name}`} />
+      </InViewLoad>
       <p className={styles.text}>{text}</p>
       <p className={styles.name}>{name}</p>
-      <Image src={mouthpieceImage} className={styles.mouthpiece} alt={'Chat bubble mouthpiece image'} placeholder='blur' />
+      <Image src={mouthpieceImage} className={styles.mouthpiece} alt='Chat bubble mouthpiece image' priority />
     </div>
   )
 }
