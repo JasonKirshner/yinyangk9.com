ae1143ce42cf6afa99c809d140c04972c1e66f1a
Jason Kirshner <jasonkirshner13@gmail.com>
2024-07-14 21:54:07 -0400

    Import about us hero image
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,28 +1,29 @@
-import HeroSmall from "@/components/HeroSmall/HeroSmall"
-import FooterCTA from "@/components/FooterCTA/FooterCTA"
-import About from "@/components/About/About"
+import HeroSmall from '@/components/HeroSmall/HeroSmall'
+import FooterCTA from '@/components/FooterCTA/FooterCTA'
+import About from '@/components/About/About'
 
 import aboutUsContent from '@/lib/data/aboutUs.json'
 
 import familyImage from '../../public/family-image1.png'
 import pettingDogImage from '../../public/petting-dog.png'
+import heroAboutUs from '../../public/hero_about_us.png'
 
-export function getStaticProps() {
+export function getStaticProps () {
   return { props: { title: 'About Us' } }
 }
 
-export default function AboutUs() {
+export default function AboutUs () {
   return (
     <main>
-      <HeroSmall 
-      headerText="About Us"
-      backgroundImage="hero_about_us.png"
+      <HeroSmall
+        headerText='About Us'
+        backgroundImage={heroAboutUs}
       />
-      <About 
-      title="Jordyn Siordia and Dylan Baquero"
-      paragraphs={aboutUsContent}
-      image1={familyImage}
-      image2={pettingDogImage}
+      <About
+        title='Jordyn Siordia and Dylan Baquero'
+        paragraphs={aboutUsContent}
+        image1={familyImage}
+        image2={pettingDogImage}
       />
       <FooterCTA />
     </main>
