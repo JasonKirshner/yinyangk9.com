7e41bd5b328e25a27b8b06ef94884b7975acca88
Jason Kirshner <jasonkirshner13@gmail.com>
2024-07-26 00:41:18 -0400

    Add all favicon variants
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -29,7 +29,13 @@ export default function MyApp ({ Component, pageProps }) {
           training and group classes in and around San Diego.'
         />
         <meta property='og:title' content={pageProps.title} />
-        <link rel='icon' type='image/x-icon' href='/favicon.ico' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/favicons/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicons/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicons/favicon-16x16.png' />
+        <link rel='manifest' href='/favicons/site.webmanifest' />
+        <link rel='mask-icon' href='/favicons/safari-pinned-tab.svg' color='#5bbad5' />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name='theme-color' content='#ffffff' />
       </Head>
       <NextNProgress color='#D52129' height={4} options={{ showSpinner: false }} />
       <Navbar />
