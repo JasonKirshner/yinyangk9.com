25cb946183df9129242602426ec406713b6584c2
Jason Kirshner <jasonkirshner13@gmail.com>
2024-07-30 16:58:50 -0400

    Finish styling about us page
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,22 +3,31 @@ import Image from 'next/image'
 import styles from './About.module.css'
 import InViewLoad from '../InViewLoad/InViewLoad'
 
-const About = ({ title, paragraphs, image1, image2 }) => {
+const About = ({ title, paragraphs, image1, image2, paws }) => {
   return (
-    <div className={styles.about}>
-      <div className={styles.text}>
-        <h2>{title}</h2>
-        {paragraphs.map((para, index) => (
-          <p key={index}>{para}</p>
-        ))}
-      </div>
-      <div className={styles.images}>
+    <div className={`container ${styles.about}`}>
+      <div className={styles.content}>
+        <div className={styles.text}>
+          <h3 className={styles.title}>{title}</h3>
+          <p>{paragraphs[0]}</p>
+          <p>{paragraphs[1]}</p>
+        </div>
         <InViewLoad>
           <Image className={styles.img} src={image1} alt='Family Image' />
         </InViewLoad>
+      </div>
+      <InViewLoad>
+        <Image className={styles.paws} src={paws} alt='Paw prints' />
+      </InViewLoad>
+      <div className={styles.content}>
         <InViewLoad>
           <Image className={styles.img} src={image2} alt='Petting Dog Image' />
         </InViewLoad>
+        <div className={styles.text}>
+          <p>{paragraphs[2]}</p>
+          <p>{paragraphs[3]}</p>
+          <p>{paragraphs[4]}</p>
+        </div>
       </div>
     </div>
   )
