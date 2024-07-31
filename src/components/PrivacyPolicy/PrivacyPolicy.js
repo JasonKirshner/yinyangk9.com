019e3d35c3824585d17df884efa1e628bbc881f9
Jason Kirshner <jasonkirshner13@gmail.com>
2024-07-30 23:43:10 -0400

    Add wysiwyg
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- src/components/PrivacyPolicy/PrivacyPolicy.js
+++ src/components/PrivacyPolicy/PrivacyPolicy.js
@@ -2,7 +2,7 @@ import styles from './PrivacyPolicy.module.css'
 
 const PrivacyPolicy = () => {
   return (
-    <div className={`container ${styles.policy}`}>
+    <div className={`container wysiwyg ${styles.policy}`}>
       <h1>Privacy Policy</h1>
       <p>Last updated: July 31, 2024</p>
       <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
