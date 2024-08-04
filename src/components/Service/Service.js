b1e7e234b69144c30c9c1138ba117cd6434d6e68
Bradley <bradkirsh03@gmail.com>
2024-08-04 17:21:18 -0400

    add link component to redirect learn more button

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -1,13 +1,18 @@
 import styles from './Service.module.css'
+import Link from 'next/link'
 
 const Service = ({ title, description, id }) => {
   return (
     <div className={styles.service}>
       <h4 className={styles.title}>{title}</h4>
       <p className={styles.description}>{description}</p>
-      <button className={`button ${styles.serviceButton} ${id % 3 === 0 ? 'button' : 'button--alt'}`}>
-        Learn More
-      </button>
+      {/* <Link className={`button ${styles.contactUsBtn}`} href='/services'> */}
+      {/* To go to part of servcies page you want, maybe add hrefId to pass to button in service/services */}
+      <Link href='/services'>
+        <button className={`button ${styles.serviceButton} ${id % 3 === 0 ? 'button' : 'button--alt'}`}>
+          Learn More
+        </button>
+      </Link>
     </div>
   )
 }
