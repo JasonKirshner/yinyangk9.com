003a6e9028d004fdd29b873ff56f07ff579d0e42
Jason Kirshner <jasonkirshner13@gmail.com>
2024-08-08 00:53:16 -0400

    Add form validation
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,168 +1,239 @@
-"use client";
+'use client'
 
-import { useState } from "react";
-import { useSearchParams } from "next/navigation";
-import { Input, Textarea, Modal, ModalContent, ModalHeader, ModalFooter, ModalBody, Button } from "@nextui-org/react";
+import { useState, useMemo } from 'react'
+import { useSearchParams } from 'next/navigation'
+import { Input, Textarea, Modal, ModalContent, ModalHeader, ModalFooter, ModalBody, Button } from '@nextui-org/react'
 
-import { responseErrorHandler } from "@/lib/js/util";
-
-import styles from "./ContactForm.module.css";
+import styles from './ContactForm.module.css'
 
 const ContactUsForm = () => {
-  const searchParams = useSearchParams();
-  const queryParamService = searchParams.get("service");
+  const searchParams = useSearchParams()
+  const queryParamService = searchParams.get('service')
 
-  const [ownersName, setOwnersName] = useState("");
-  const [dogName, setDogName] = useState("");
-  const [message, setMessage] = useState("");
-  const [phone, setPhone] = useState("");
-  const [email, setEmail] = useState("");
-  const [service, setService] = useState(queryParamService || "");
+  const [ownersName, setOwnersName] = useState('')
+  const [dogName, setDogName] = useState('')
+  const [message, setMessage] = useState('')
+  const [phone, setPhone] = useState('')
+  const [email, setEmail] = useState('')
+  const [ownersNameError, setOwnersNameError] = useState(false)
+  const [dogsNameError, setDogsNameError] = useState(false)
+  const [emailError, setEmailError] = useState(false)
+  const [service, setService] = useState(queryParamService || 'select-a-service')
 
-  const [responseMessage, setResponseMessage] = useState("");
-  const [responseTitle, setResponseTitle] = useState("");
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [responseMessage, setResponseMessage] = useState('')
+  const [responseTitle, setResponseTitle] = useState('')
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const validateEmail = (value) => {
+    return value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i) !== null
+  }
 
-    const response = await fetch("/api/send-mail", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ ownersName, dogName, phone, email, message, service }),
-    });
+  const validatePhoneNumber = (value) => {
+    return value.match(/^[+]?(1-|1\s|1|\d{3}-|\d{3}\s|)?((\(\d{3}\))|\d{3})(-|\s)?(\d{3})(-|\s)?(\d{4})$/g) !== null
+  }
 
-    const data = await response.json();
+  const isEmailInvalid = useMemo(() => {
+    if (email === '') return false
 
-    if (response.ok) {
-      setResponseTitle(data.title);
-      setResponseMessage(data.message);
-    } else {
-      setResponseTitle(data.title);
-      setResponseMessage(data.message);
+    if (emailError) {
+      setEmailError(false)
     }
 
-    setIsModalOpen(true);
+    return !validateEmail(email)
+  }, [email, emailError])
 
-    // responseErrorHandler(response)
-  };
+  const isPhoneInvalid = useMemo(() => {
+    if (phone === '') return false
 
-  const handleChange = (event) => {
-    setService(event.target.value);
-  };
+    return !validatePhoneNumber(phone)
+  }, [phone])
+
+  const validateForm = () => {
+    let failedValidation = false
+
+    if (!validateEmail(email)) {
+      setEmailError(true)
+      failedValidation = true
+    }
+
+    if (ownersName === '') {
+      setOwnersNameError(true)
+      failedValidation = true
+    }
+
+    if (dogName === '') {
+      setDogsNameError(true)
+      failedValidation = true
+    }
+
+    return failedValidation
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if (validateForm()) {
+      return
+    }
+
+    const response = await fetch('/api/send-mail', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ ownersName, dogName, phone, email, message, service })
+    })
+
+    const data = await response.json()
+
+    if (response.ok) {
+      setResponseTitle(data.title)
+      setResponseMessage(data.message)
+    } else {
+      setResponseTitle(data.title)
+      setResponseMessage(data.message)
+    }
+
+    setIsModalOpen(true)
+  }
+
+  const handleServiceChange = (event) => {
+    setService(event.target.value)
+  }
 
   const closeModal = () => {
-    setIsModalOpen(false);
-  };
+    setIsModalOpen(false)
+  }
 
   return (
-    <div id="contactForm" className={`container ${styles.contactContainer}`}>
-      <h3 className="h3">Lets hear about you pup!</h3>
+    <div id='contactForm' className={`container ${styles.contactContainer}`}>
+      <h3 className='h3'>Lets hear about you pup!</h3>
       <form onSubmit={handleSubmit} className={styles.form}>
         <Input
           required
-          id="ownersName"
-          type="text"
+          id='ownersName'
+          type='text'
           value={ownersName}
-          onChange={(e) => setOwnersName(e.target.value)}
+          onChange={() => {
+            if (ownersNameError) {
+              setOwnersNameError(false)
+            }
+          }}
+          isInvalid={ownersNameError}
+          onValueChange={setOwnersName}
           label="Owner's Name"
+          errorMessage='Please enter a name'
           classNames={{
             base: styles.fieldWrapper,
-            label: [styles.label, "input-label"],
-            input: styles.input,
+            label: [styles.label, styles.ownersNameLabel, styles.required, 'input-label'],
+            input: [styles.input, ownersNameError && styles.inputError],
+            errorMessage: styles.errorMessage
           }}
           fullWidth
         />
         <Input
           required
-          id="dogsName"
-          type="text"
+          id='dogsName'
+          type='text'
           label="Dog's Name"
           value={dogName}
-          onChange={(e) => setDogName(e.target.value)}
+          onValueChange={setDogName}
+          isInvalid={dogsNameError}
+          errorMessage='Please enter a name'
+          onChange={() => {
+            if (dogsNameError) {
+              setDogsNameError(false)
+            }
+          }}
           classNames={{
             base: styles.fieldWrapper,
-            label: [styles.label, "input-label"],
-            input: styles.input,
+            label: [styles.label, styles.required, 'input-label'],
+            input: [styles.input, dogsNameError && styles.inputError],
+            errorMessage: styles.errorMessage
           }}
           fullWidth
         />
         <Input
           required
-          id="phone"
-          type="tel"
-          label="Phone Number"
+          id='phone'
+          type='tel'
+          label='Phone Number'
           value={phone}
-          onChange={(e) => setPhone(e.target.value)}
+          isInvalid={isPhoneInvalid}
+          errorMessage='Please enter a valid Phone Number'
+          onValueChange={setPhone}
           classNames={{
             base: styles.fieldWrapper,
-            label: [styles.label, "input-label"],
-            input: styles.input,
+            label: [styles.label, 'input-label'],
+            input: [styles.input, isPhoneInvalid && styles.inputError],
+            errorMessage: styles.errorMessage
           }}
           fullWidth
         />
         <Input
           required
-          id="email"
-          type="email"
-          label="Email"
+          id='email'
+          type='email'
+          label='Email'
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          onValueChange={setEmail}
+          isInvalid={isEmailInvalid || emailError}
+          errorMessage='Please enter a valid email'
           classNames={{
             base: styles.fieldWrapper,
-            label: [styles.label, "input-label"],
-            input: styles.input,
+            label: [styles.label, styles.required, 'input-label'],
+            input: [styles.input, (isEmailInvalid || emailError) && styles.inputError],
+            errorMessage: styles.errorMessage
           }}
           fullWidth
         />
         <div className={styles.serviceDropDownWrapper}>
           <label
-            className={`input-label ${styles.label}`}
-            htmlFor="service-dropdown"
+            className={`input-label ${styles.label} ${styles.required}`}
+            htmlFor='service-dropdown'
           >
             Service
           </label>
           <select
-            id="service-dropdown"
+            id='service-dropdown'
             className={styles.serviceDropDown}
             value={service}
-            onChange={handleChange}
+            onChange={handleServiceChange}
           >
-            <option value="board-n-train">Board & Train</option>
-            <option value="boarding">Boarding</option>
-            <option value="train-n-play">Train & Play</option>
-            <option value="private-lessons">Private Lessons</option>
+            <option value='select-a-service' selected disabled>Select a service</option>
+            <option value='board-n-train'>Board & Train</option>
+            <option value='boarding'>Boarding</option>
+            <option value='train-n-play'>Train & Play</option>
+            <option value='private-lessons'>Private Lessons</option>
           </select>
           <svg
             className={styles.chevron}
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 16 16"
+            xmlns='http://www.w3.org/2000/svg'
+            fill='currentColor'
+            viewBox='0 0 16 16'
           >
             <path
-              fillRule="evenodd"
-              d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
+              fillRule='evenodd'
+              d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708'
             />
           </svg>
         </div>
         <Textarea
           require
-          id="message"
+          id='message'
           minRows={5}
-          label="How Can We Help?"
+          label='How Can We Help?'
           value={message}
           onChange={(e) => setMessage(e.target.value)}
           classNames={{
             base: styles.fieldWrapper,
-            label: [styles.label, "input-label"],
-            input: styles.textarea,
+            label: [styles.label, 'input-label'],
+            input: styles.textarea
           }}
           fullWidth
         />
         <button
-          type="submit"
+          type='submit'
           className={`button ${styles.sendBtn}`}
           onClick={handleSubmit}
         >
@@ -174,7 +245,7 @@ const ContactUsForm = () => {
         isOpen={isModalOpen}
         onClose={closeModal}
         isDismissable={false}
-        isKeyboardDismissDisabled={true}
+        isKeyboardDismissDisabled
         className={styles.modal}
         classNames={{
           closeButton: styles.closeButton,
@@ -191,7 +262,7 @@ const ContactUsForm = () => {
                 <p>{responseMessage}</p>
               </ModalBody>
               <ModalFooter className={styles.modalFooter}>
-                <Button className='button' color="danger" variant="light" onPress={onClose}>
+                <Button className='button' color='danger' variant='light' onPress={onClose}>
                   Close
                 </Button>
               </ModalFooter>
@@ -200,7 +271,7 @@ const ContactUsForm = () => {
         </ModalContent>
       </Modal>
     </div>
-  );
-};
+  )
+}
 
-export default ContactUsForm;
+export default ContactUsForm
