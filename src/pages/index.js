e25ea26d7daad0013172f10a29bdc8fbb78b15f3
Jason Kirshner <jasonkirshner13@gmail.com>
2024-10-11 21:29:31 -0400

    Update images
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ import { validation } from '@/lib/js/util'
 
 import heroImage from '../../public/hero_right.png'
 import bgImage from '../../public/hero_bg.png'
+import instagramLogo from '../../public/instagram_logo.png'
 
 // Testimonial avatars
 import zara from '../../public/zara.jpeg'
@@ -76,13 +77,13 @@ export default function Home ({ instagramFeed }) {
         image={heroImage}
         bgImage={bgImage}
         title='Expert Dog Training'
-        description='Yin Yang K9 is based in San Diego, California and offers a variety of private in home dog training, day training and group classes in and around San Diego.'
+        description='Yin Yang K9 is based in San Diego, California. Offering a variety of private dog training, day training and group classes in and around San Diego.'
         buttonText1='Contact Us'
         buttonText2='About Us'
       />
       <Testimonials title='Testimonials' testimonials={testimonials} />
       <Services title='Services' services={services} />
-      {validation(instagramFeed) && <InstagramFeed instagramFeed={instagramFeed} />}
+      {validation(instagramFeed) && <InstagramFeed instagramFeed={instagramFeed} logo={instagramLogo} />}
       <FooterCTA />
     </main>
   )
